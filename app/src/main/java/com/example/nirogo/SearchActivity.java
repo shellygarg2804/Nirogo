b9ac15f48cb427759ba3927e2294ec89399f964f
mishra5047 <mishra695047@gmail.com>
2020-06-03 23:45:59 +0530

    Ambulance Screen

---
+++ app/src/main/java/com/example/nirogo/SearchActivity.java
@@ -0,0 +1,17 @@
+package com.example.nirogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+
+    }
+}
